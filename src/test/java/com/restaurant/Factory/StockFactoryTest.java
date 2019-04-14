5ac5df2afeabb65b313b961f66a8a58a7b23df8a
BaneleMlamleli <mlamlelibanele@yahoo.com>
2019-04-14 21:23:01 +0200

    Added test classes

--- src/test/java/com/restaurant/Factory/StockFactoryTest.java
+++ src/test/java/com/restaurant/Factory/StockFactoryTest.java
@@ -1,4 +1,14 @@
 package com.restaurant.Factory;
 
+import org.junit.Test;
+
 public class StockFactoryTest {
+
+    @Test
+    public void getStock(){
+
+    }
+    @Test
+    public void getItemName() {
+    }
 }
