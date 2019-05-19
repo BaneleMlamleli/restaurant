4ea942ec9d23f92f254ad41cf7143cb4bb7ba330
BaneleMlamleli <mlamlelibanele@yahoo.com>
2019-05-19 14:54:00 +0200

    Added a landing/default end-point in the IndexController that prints "Welcome to restaurant app"

--- src/main/java/com/restaurant/Controller/IndexController.java
+++ src/main/java/com/restaurant/Controller/IndexController.java
@@ -1,12 +1,15 @@
 package com.restaurant.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
-    @RequestMapping("/")
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+    @GetMapping("/")
     String index(){
-        return "index";
+        return welcomeMessage;
     }
 }
